094d1e83371da1a23ff81620aeb644fba4b9e0ec
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-15 22:58:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,14 @@ class App < Sinatra::Base
     "#{params[:word1]+params[:word2]+params[:word3]+params[:word4]+params[:word5]}"
   end
   
-  get 
+  get '/:operation/:number1/:number2' do 
+    @operation=params[:operation]
+    @number1=params[:number1].to_i
+    @number2=params[:number2].to_i
+    @solution=@number1 @operation @number2
+    "#{@solution}"
+    
+  end 
   
   
 
