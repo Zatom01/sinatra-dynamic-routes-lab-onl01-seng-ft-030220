757f029ee2c01aa3b410529d72008bc12fe44b2f
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-15 23:10:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,18 @@ class App < Sinatra::Base
     
     if @operation=="add"
       "#{@number1+@number2}"
+      
+    elsif @operation =="subtract"
+      "#{@number-@number2}"
+      
+    elsif @operation=="multiply"
+      "#{@number1 * @number2}"
+    
+    elsif @operation == "divide"
+      "#{@number1 / @number2}"    
+      
     end 
+    
     
   end 
   
