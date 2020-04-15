1cab317678e9567f6c6b20a895395f1792af9d2e
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-15 23:07:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,8 +29,10 @@ class App < Sinatra::Base
     @operation=params[:operation]
     @number1=params[:number1].to_i
     @number2=params[:number2].to_i
-    @solution=@number1 @operation @number2
-    "#{@solution}"
+    
+    if @operation=="add"
+      "#{@number1+@number2}"
+    end 
     
   end 
   
